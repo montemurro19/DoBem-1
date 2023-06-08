b48cf599bcac627e6e3d5cc4710de95fe0f781f7
montemurro19 <montemurro.matheus@gmail.com>
2023-06-08 01:10:13 +0000

    Co-authored-by: JulioMachadoFretes <JulioMachadoFretes@users.noreply.github.com> Co-authored-by: Victor Hugo <VicDevs@users.noreply.github.com> Co-authored-by: VitorioGraton <VitorioGraton@users.noreply.github.com>

--- DoBem/App.jsx
+++ DoBem/App.jsx
@@ -7,21 +7,43 @@ import { Home } from './screens/Home';
 import { CadastroPJ } from './screens/CadastroPJ';
 import { PaginaInicial } from './screens/PaginaInicial';
 import { CadastroOng } from './screens/CadastroOng';
+import { ListaEquipes } from './screens/Equipes';
+import { useState } from 'react'
 
 export default function App() {
 
   const Stack = createStackNavigator();
 
+  const [id, setId] = useState(null)
+  const [token, setToken] = useState(null)  
+
+  const buscarId = (paramId)=> {
+    setId(paramId)
+  }
+
+  const buscarToken = (paramToken)=> {
+    setToken(paramToken)
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Cadastro de Pessoa jurídica" component={CadastroPJ} />
-        <Stack.Screen name="Cadastro de Equipes" component={CadastroEquipe} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Cadastro" component={Cadastro} />
-        <Stack.Screen name="Pagina inicial" component={PaginaInicial} />
-        <Stack.Screen name="Cadastro Ong" component={CadastroOng} />
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="Cadastro de Pessoa jurídica" component={CadastroPJ}/>
+        <Stack.Screen name="Cadastro de Equipes">
+          {props => <CadastroEquipe {...props} id={id} token={token} navigation={props.navigation} />}
+        </Stack.Screen>
+        <Stack.Screen name="Login">
+          {props => <Login {...props} buscarId={buscarId} buscarToken={buscarToken} navigation={props.navigation} />}
+        </Stack.Screen>
+        <Stack.Screen name="Cadastro" component={Cadastro}/>
+        <Stack.Screen name="Pagina inicial" component={PaginaInicial}/>
+        <Stack.Screen name="Cadastro Ong">
+        {props => <CadastroOng {...props} token={token} navigation={props.navigation} />}
+        </Stack.Screen>
+        <Stack.Screen name="Equipes">
+          {props => <ListaEquipes {...props} token={token} navigation={props.navigation} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
